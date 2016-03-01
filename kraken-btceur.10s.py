41af13996fba50d9ca1c0a26e4ab275fbd576146
Nicolas Khadivi <nicolaskhadivi@gmail.com>
2016-03-01 19:35:58 +0100

    Updated BTCEUR script to use 'requests'.

--- kraken-btceur.10s.py
+++ kraken-btceur.10s.py
@@ -8,9 +8,13 @@
 # Shows the last price BTCEUR price from Kraken.
 
 import json
-import urllib2
+import requests
 
-data = urllib2.urlopen("https://api.kraken.com/0/public/Ticker?pair=XBTEUR").read()
-obj = json.loads(data)
+url = "https://api.kraken.com/0/public/Ticker"
+payload = {"pair": "XXBTZEUR"}
+
+response = requests.get(url, params=payload)
+obj = response.json()
+
 price = float(obj['result']['XXBTZEUR']['c'][0])
 print "€{0:.2f}".format(price)
