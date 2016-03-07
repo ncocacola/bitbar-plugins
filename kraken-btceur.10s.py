f677e4513d3f5c5084d140f7cc8a1d0deaa14fd4
Nicolas Khadivi <nicolaskhadivi@gmail.com>
2016-03-07 18:39:48 +0100

    Added day low/high prices.

--- kraken-btceur.10s.py
+++ kraken-btceur.10s.py
@@ -23,6 +23,13 @@ except:
     sys.exit(-1)
 
 obj = response.json()
+ticker = obj['result']['XXBTZEUR']
 
-price = float(obj['result']['XXBTZEUR']['c'][0])
+price = float(ticker['c'][0])
+day = (float(ticker['l'][0]), float(ticker['h'][0]))
+
 print "€{0:.2f}".format(price)
+print "---"
+print "Price: €{0:.2f}".format(price)
+print "Low: €{0:.2f}".format(day[0])
+print "High: €{0:.2f}".format(day[1])
